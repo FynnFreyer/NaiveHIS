a9856e25fc231e1263fb8b46b7c24378f7361688
Fynn Freyer <fynn.freyer@googlemail.com>
2023-02-04 18:11:56 +0100

    adjust settings for static files and csrf

--- src/NaiveHIS/settings.py
+++ src/NaiveHIS/settings.py
@@ -17,7 +17,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -38,6 +37,9 @@ INTERNAL_IPS = [
     '127.0.0.1'
 ]
 
+CSRF_TRUSTED_ORIGINS = [environment.get('HOST', 'localhost')]
+CSRF_COOKIE_DOMAIN = '.' + environment.get('HOST', 'localhost')
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -50,9 +52,7 @@ INSTALLED_APPS = [
     'NaiveHIS.apps.NaiveHisConfig',
 ]
 
-
 AUTH_USER_MODEL = 'NaiveHIS.HISAccount'
-
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -84,7 +84,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'NaiveHIS.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
@@ -94,7 +93,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -114,7 +112,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -126,11 +123,11 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
